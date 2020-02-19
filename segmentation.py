b610fd038cf0883fba2ddb7e7e7354fdb43764d1
WatcharachatMPBG <watcharachat@hotmail.com>
2020-02-19 11:50:01 +0700

    testing

--- segmentation.py
+++ segmentation.py
@@ -1,9 +1,14 @@
-import cv2
 import numpy as np
+import cv2
+import matplotlib.pyplot as plt
 
-try:
-    pass
-except expression as identifier:
-    pass
-else:
-    pass
+img = cv2.imread('testfile/testimage.jpg')
+ret,binimg = cv2.threshold(img,127,255,cv2.THRESH_BINARY) #turns image into binary
+height,width,channel = img.shape
+
+
+'''
+cv2.imshow('image',blur)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+'''
