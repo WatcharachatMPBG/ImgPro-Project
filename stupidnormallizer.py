6c9e3cdf0e65399417b9bb2bc10fdf6a10e2527b
WatcharachatMPBG <watcharachat@hotmail.com>
2020-02-26 15:36:44 +0700

    added stupid normalizer

---
+++ stupidnormallizer.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import argparse
+import os
+import shutil
+import os
+
+def normalize(img):
+    blank_image = np.zeros((100,100,3), np.uint8)
+    width,height,channel = img.shape
+    for x in range(width):
+        for y in range(height):
+            blank_image[x,y,0] = img[x,y,0]
+            blank_image[x,y,1] = img[x,y,1]
+            blank_image[x,y,2] = img[x,y,2]
+    return blank_image
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+args = vars(ap.parse_args())
+
+img = cv2.imread(args["image"])
+
+img = normalize(img)
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
