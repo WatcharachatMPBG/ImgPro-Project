a1b5619f81ec5fb464f754e7b590a7dea52f0878
WatcharachatMPBG <watcharachat@hotmail.com>
2020-03-20 14:36:13 +0700

    restore to LibV1

--- 2ndrefactor/flatteninggen.py
+++ 2ndrefactor/flatteninggen.py
@@ -15,12 +15,12 @@ def main():
     intClassifications = []
     npaFlattenedImages =  np.empty((0, RESIZED_IMAGE_WIDTH * RESIZED_IMAGE_HEIGHT))
 
-    
+    '''
     #init dictionary
     dictionary = {"dictcount":0}
     json.dump(dictionary, open("dictThaitoNum.txt",'w'))
     json.dump(dictionary, open("dictNumtoThai.txt",'w'))
-    
+    '''
     '''
     #open dictionary
     d2 = json.load(open("text.txt"))
@@ -70,10 +70,10 @@ def main():
 
     fltClassifications = np.array(intClassifications, np.float32)                   # convert classifications list of ints to numpy array of floats
     npaClassifications = fltClassifications.reshape((fltClassifications.size, 1))   # flatten numpy array of floats to 1d so we can write to file later
-
-
+    
     np.savetxt("classificationsV2.txt", npaClassifications)           # write flattened images to file
     np.savetxt("flattened_imagesV2.txt", npaFlattenedImages)
+    
     json.dump(dictThaitoNum, open("dictThaitoNum.txt",'w'))
     json.dump(dictNumtoThai, open("dictNumtoThai.txt",'w'))
     cv2.destroyAllWindows()
